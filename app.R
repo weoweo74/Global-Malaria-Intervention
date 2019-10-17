6a71c2cfba152adfe85327663fa009f026a306c6
Williams <izehieghogho@gmail.com>
2019-10-17 13:26:02 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,18 +1,4 @@
 # Create shiny app in one single script togrther for both UI and SERVER
-# Install devtools, if you haven't already.
-install.packages("devtools")
-
-# install devtools
-install.packages("devtools")
-# install ggmap from dev space
-devtools::install_github("dkahle/ggmap")
-
-library(ggmap)
-
-############WORK VIA GITHUB is below##############
-devtools::install_github("trestletech/shinyGlobe", force = TRUE)
-install.packages("shinyGlobe")
-#githubinstall("shinyGlobe")
 library(shiny)
 #library(shinyGlobe)
 library(ggplot2)
@@ -28,6 +14,21 @@ library(leaflet)
 library(shinyjs)
 library(V8)
 library(reshape)
+# Install devtools, if you haven't already.
+install.packages("devtools")
+
+# install devtools
+install.packages("devtools")
+# install ggmap from dev space
+devtools::install_github("dkahle/ggmap")
+
+library(ggmap)
+
+############WORK VIA GITHUB is below##############
+devtools::install_github("trestletech/shinyGlobe", force = TRUE)
+install.packages("shinyGlobe")
+#githubinstall("shinyGlobe")
+
 
 
 
